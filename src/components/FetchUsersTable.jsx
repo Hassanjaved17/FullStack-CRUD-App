4572614ea9f6f77b30cbd8e92e868a5331bce6c2
Hassanjaved17 <hassanaptech9@gmail.com>
2026-02-17 22:58:41 +0500

    Refactor FetchUsersTable component for improved readability and structure

--- src/components/FetchUsersTable.jsx
+++ src/components/FetchUsersTable.jsx
@@ -1,58 +1,50 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useEffect } from 'react';
 
 const FetchUsersTable = () => {
   const [users, setUsers] = useState([]);
+
   const fetchUsers = async () => {
     try {
       const response = await axios.get("http://localhost:3000/getuser");
-      console.log(response.data.users);
-      setUsers(response.data.users);   
-    }
-    catch (err) {
+      setUsers(response.data.users);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
- 
+
   useEffect(() => {
     fetchUsers();
-  }, [users]);
-  
+  },[users]);
+
   return (
-    <>
-      <div className="overflow-x-auto w-[50%] mx-auto my-20 shadow-sm">
-        <table className="table table-zebra">
-          {/* head */}
-          <thead>
-            <tr>
-              <th>Index</th>
-              <th>Name</th>
-              <th>Message</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            { users.map((user, index) =>{
-              return(
-            <tr>
+    <div className="overflow-x-auto w-[50%] mx-auto my-20 shadow-sm">
+      <table className="table table-zebra">
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>Name</th>
+            <th>Message</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user._id || index}>
               <th>{index + 1}</th>
               <td>{user.username}</td>
               <td>{user.message}</td>
-         <td>
-          <button className="btn btn-soft btn-info btn-sm">Edit</button>
-          <button className="btn btn-soft btn-error btn-sm ml-2">Delete </button>
-         </td>
+              <td>
+                <button className="btn btn-soft btn-info btn-sm">Edit</button>
+                <button className="btn btn-soft btn-error btn-sm ml-2">Delete</button>
+              </td>
             </tr>
-            );
-             })}
-          </tbody>
-        </table>
-      </div>
-    </>
-  )
-}
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default FetchUsersTable
+export default FetchUsersTable;
